c654290917674a7180e925c9dea1b3ece487ed2c
SiMista <78701925+SiMista@users.noreply.github.com>
2021-10-03 20:42:19 +0200

    Commencement de la méthode en prog dynamique

--- src/appli/SacADos.java
+++ src/appli/SacADos.java
@@ -12,20 +12,19 @@ import java.util.regex.Pattern;
 
 public class SacADos {
 	private ArrayList<Objet> sac;
-	private ArrayList<Objet> listeObjet;
+	private ArrayList<Objet> listeObjets;
 	private float poidsMax;
-	private float poidsActuel;
-	
+	public float poidsActuel; // CHANGER APRES !!
 
 	public SacADos() {
 		sac = new ArrayList<>();
-		listeObjet = new ArrayList<Objet>();
+		listeObjets = new ArrayList<Objet>();
 		poidsActuel = 0;
 	}
 
 	public SacADos(String chemin, float poidsMax) throws NumberFormatException, IOException {
 		sac = new ArrayList<>();
-		listeObjet = new ArrayList<Objet>();
+		listeObjets = new ArrayList<Objet>();
 		poidsActuel = 0;
 		BufferedReader in = new BufferedReader(new FileReader(chemin));
 		String line;
@@ -34,14 +33,14 @@ public class SacADos {
 				return;
 			StringTokenizer stk = new StringTokenizer(line, ";");
 			Objet obj = new Objet(stk.nextToken(), Float.valueOf(stk.nextToken()), Float.valueOf(stk.nextToken()));
-			listeObjet.add(obj);
+			listeObjets.add(obj);
 		}
 		in.close();
 		this.poidsMax = poidsMax;
-		
+
 		System.out.println("   Objets ajoute dans la liste\n");
 		float poids = 0;
-		for (Objet o : listeObjet) {
+		for (Objet o : listeObjets) {
 			System.out.println(o.getNom());
 			System.out.println(o.getPoids());
 			System.out.println(o.getPrix());
@@ -49,66 +48,7 @@ public class SacADos {
 		}
 		System.out.println("\n Voici le poids de la liste d'objets " + poids);
 	}
-	
-	public void resoudre(String s) {
-		switch (s) {
-		case "gloutonne":
-			glouttonne();
-		}
-	}
-	
-	public void ajouter (Objet o) {
-		poidsActuel += listeObjet.get(0).getPoids();
-		sac.add(listeObjet.get(0));
-	}
-	
-	public void glouttonne() {
-		ArrayList<Objet> tmp = triRapide(this.listeObjet);
-		for (int i = 0; i<tmp.size()-1; i++) {
-			//System.out.println(tmp.get(i).getPrix()/tmp.get(i).getPoids());
-			}
-		while (!this.listeObjet.isEmpty()) {
-				this.poidsActuel += this.listeObjet.get(0).getPoids();
-				if(this.poidsActuel<=poidsMax) {
-					this.sac.add(this.listeObjet.get(0));
-					this.listeObjet.remove(0);
-				}
-				else break;
-			}
-		}
-	
-	public int repartition(ArrayList<Objet> T, int premier, int dernier, int pivot) {
-		Collections.swap(T, pivot, dernier);
-		int j = premier;
-		for(int i = premier; i<dernier-1; i++) {
-			if(T.get(i).getPrix()/T.get(i).getPoids()>=T.get(dernier).getPrix()/T.get(dernier).getPoids()) {
-				Collections.swap(T, i, j);
-				j += 1;
-			}
-		}
-		Collections.swap(T, dernier, j);
-		return j;
-	}
-	
-	public ArrayList<Objet> triRapideRec(ArrayList<Objet> T, int premier, int dernier){
-		if (premier<dernier) {
-			int pivot = (premier + dernier)/2;
-			int nouvPivot = repartition(T, premier, dernier, pivot);
-			triRapideRec(T, premier, nouvPivot-1);
-			triRapideRec(T, nouvPivot+1, dernier);
-		}
-		return T;
-	}
-	
-	public ArrayList<Objet> triRapide(ArrayList<Objet> T){
-		return triRapideRec(T, 0, T.size()-1);
-	}
-		
-	public ArrayList<Objet> getSac(){
-		return this.sac;
-	}
-	
-	// Pas sûr que cette méthode soit nécessaire
+
 	public boolean vérifEntrée(String s) {
 		// Regular expression: ajouter mots et chiffre à espace + chiffre avec x.0
 		if (!Pattern.matches("^[\\w\\s]{1,} ;{1} [\\d]{1,}[.][\\d]{1,} ;{1} [\\d]{1,}[.][\\d]{1,}$", s)) {
@@ -116,5 +56,129 @@ public class SacADos {
 			return true;
 		}
 		return false;
-	}	
+	}
+
+	public void ajouterAuSac(int i) {
+		poidsActuel += listeObjets.get(i).getPoids();
+		sac.add(listeObjets.get(i));
+	}
+
+	public void glouttonne() {
+		ArrayList<Objet> tmp = triRapide(this.listeObjets);
+		for (int i = 0; i < tmp.size() - 1; i++) {
+			 // System.out.println(tmp.get(i).getPrix()/tmp.get(i).getPoids());
+		}
+		while (!this.listeObjets.isEmpty()) {
+			if(this.poidsActuel + this.listeObjets.get(0).getPoids() <=poidsMax) {
+				ajouterAuSac(0);
+				listeObjets.remove(0);
+			}
+			else break;
+		}
+	}
+
+	public int repartition(ArrayList<Objet> T, int premier, int dernier, int pivot) {
+		Collections.swap(T, pivot, dernier);
+		int j = premier;
+		for (int i = premier; i < dernier - 1; i++) {
+			if (T.get(i).getPrix() / T.get(i).getPoids() >= T.get(dernier).getPrix() / T.get(dernier).getPoids()) {
+				Collections.swap(T, i, j);
+				j += 1;
+			}
+		}
+		Collections.swap(T, dernier, j);
+		return j;
+	}
+
+	public ArrayList<Objet> triRapideRec(ArrayList<Objet> T, int premier, int dernier) {
+		if (premier < dernier) {
+			int pivot = (premier + dernier) / 2;
+			int nouvPivot = repartition(T, premier, dernier, pivot);
+			triRapideRec(T, premier, nouvPivot - 1);
+			triRapideRec(T, nouvPivot + 1, dernier);
+		}
+		return T;
+	}
+
+	public ArrayList<Objet> triRapide(ArrayList<Objet> T) {
+		return triRapideRec(T, 0, T.size() - 1);
+	}
+
+	public void progDynamique() {
+		int ligneMax = listeObjets.size();
+		int colonneMax = (int) poidsMax + 1;
+		float tabVal[][] = new float[ligneMax][colonneMax];
+
+		for (int j = 0; j < colonneMax; ++j) {
+			if (listeObjets.get(0).getPoids() > j)
+				tabVal[0][j] = 0;
+			else
+				tabVal[0][j] = listeObjets.get(0).getPrix();
+		}
+
+		for (int i = 1; i < ligneMax; ++i) {
+			for (int j = 0; j < colonneMax; ++j) {
+				if (listeObjets.get(i).getPoids() > j)
+					tabVal[i][j] = tabVal[i - 1][j];
+				else
+					tabVal[i][j] = maxEntre(tabVal[i - 1][j],
+							tabVal[i - 1][(int) (j - listeObjets.get(i).getPoids())] + listeObjets.get(i).getPrix());
+			}
+		}
+		/*
+		for (; colonneMax > 0; --colonneMax) {
+			if (tabVal[ligneMax - 1][colonneMax] != tabVal[ligneMax - 1][colonneMax - 1])
+				break;
+		}
+
+		for (; colonneMax > 0; --colonneMax) {
+			for (; ligneMax > 0; --ligneMax) {
+				if (tabVal[ligneMax - 1][colonneMax] != tabVal[ligneMax - 2][colonneMax]) {
+					colonneMax -= listeObjets.get(ligneMax - 1).getPoids();
+					if (colonneMax >= 0) {
+						ajouterAuSac(ligneMax - 1);
+						--ligneMax;
+					}
+					break;
+				}
+			}
+		}
+		*/
+		int i = ligneMax - 1;
+		int j = colonneMax - 1;
+		while (tabVal[i][j] == tabVal[i][j - 1])
+			--j;
+		System.out.println(j);
+		while (j > 0) {
+			while (i > 0 && tabVal[i][j] == tabVal[i - 1][j])
+				--i;
+			j = (int) (j - listeObjets.get(i).getPoids());
+			System.out.println(j);
+			if (j > 0)
+				ajouterAuSac(i);
+			--i;
+		}
+		
+		for (int l = 0; l < 9; ++l) {
+			for (int c = 0; c < poidsMax + 1; ++c) {
+				System.out.print(tabVal[l][c]);
+				System.out.print(" ");
+			}
+			System.out.println("");
+		}
+		
+	}
+
+	public float maxEntre(float a, float b) {
+		if (a > b)
+			return a;
+		return b;
+	}
+
+	public ArrayList<Objet> getSac() {
+		return this.sac;
+	}
+
+	// Pas sûr que cette méthode soit nécessaire
+
 }
--- src/appli/main.java
+++ src/appli/main.java
@@ -6,19 +6,17 @@ public class main {
 
 	public static void main(String[] args) throws IOException {
 		// Siméon chemin
-		SacADos sacADos = new SacADos("C:\\Users\\simeo\\OneDrive\\Bureau\\ListeObj.txt", 17);
+		SacADos sacADos = new SacADos("C:\\Users\\simeo\\OneDrive\\Bureau\\ListeObj.txt", 5);
 		// Niru chemin
 		//SacADos sacADos = new SacADos("C:\\Users\\sniru\\eclipse-workspace\\porjetAAV\\ListeObj.txt", 7);
-		sacADos.glouttonne();
+		sacADos.progDynamique();
 		// Syso pour montrer tout les objets dans le sac
 		System.out.println("\n   Objets dans le sac\n");
-		float rapport = 0;
 		for (Objet obj : sacADos.getSac()) {
 			System.out.println(obj.getNom());
 			System.out.println(obj.getPoids());
-			rapport += obj.getPoids();
 		}
-		System.out.println("\n Voici le poids actuel du sac " + rapport);
+		System.out.println("\n Voici le poids actuel du sac " + sacADos.poidsActuel);
 		
 	}
 }
